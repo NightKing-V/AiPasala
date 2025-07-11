cd235ed0c90f4e0db5ee25ab76e97c737dffa5f9
Valenteno Lenora <valentenocavlenora@gmail.com>
2025-07-11 18:38:01 +0530

    frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// frontend/next.config.js
+module.exports = {
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000,            // poll every second
+      aggregateTimeout: 300, // debounce time
+    };
+    return config;
+  },
+};
+
+
 export default nextConfig;
